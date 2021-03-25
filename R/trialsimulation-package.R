d2b56fa2f5f541ca8f0b18a28608574e3f7a4641
Jiawei Xu <51835772+JiaweiXu-code@users.noreply.github.com>
2021-03-25 13:00:06 -0400

    Update trialsimulation-package.R

--- R/trialsimulation-package.R
+++ R/trialsimulation-package.R
@@ -7,7 +7,7 @@ NULL
 
 #' Perform a simple trial simulation
 #' @param phi         -- Substantial Evidence Threshold 
-#' @param targetDelta -- Hypothesized treatment effect (T:P increase from week 6 to week 8) 
+#' @param targetDelta -- Hypothesized treatment effect 
 #' @param block0      -- Randomization information; note 1 = treatment, 2 = control
 #' @param skp_mn      -- mean of skeptical prior 
 #' @param nMin   -- minimum to trigger the analysis
@@ -18,13 +18,37 @@ NULL
 #' @param asc_mn -- mean, 8 weeks +/- 1 week
 #' @param asc_sd -- standard deviation 
 #' @param true_sd -- true standard deviation
-#' @param true_mn -- true mean change from week 6 to week 8 in T:T arm
+#' @param true_mn -- true mean change
 #' @param nSims -- number of simulations 
 #' @param nSamp -- number of MCMC samples
 #' @param hst_n  -- sample size
-#' @param hst_mn -- azm cfb mean (also mean of normal power prior)
-#' @param hst_sd -- azm sd for cfb
+#' @param hst_mn -- mean (also mean of normal power prior)
+#' @param hst_sd -- standard deviation
 #' @return a data frame with results
+#' @param skp_tau  -- inverse of standard deviation   
+#' @param skp_mu1  -- effect of control arm 
+#' @param skp_mu0  -- effect of treated arm
+#' @param skp_diff -- treatment effect
+#' @param skp_pp   -- posterior probability of activity
+#' @param ent_tau  -- inverse of standard deviation   
+#' @param ent_mu1  -- effect of control arm
+#' @param ent_mu0  -- effect of treated arm 
+#' @param ent_diff -- treatment effect
+#' @param ent_pp   -- posterior probability of activity
+#' @param analysis -- number of analysis performed
+#' @param nInt     -- Number of patients at interim where early stoppage takes place
+#' @param nOngoing -- Number of patients at interim where early stoppage doesn't take place
+#' @param nFin     -- Number of patients currently enrolled    
+#' @param essFin   -- effective sample size   
+#' @param y1Fin    -- estimated effect of control group    
+#' @param y2Fin    -- estimated treatment effect    
+#' @param fut      -- percentage of futility stopping [interim only]  
+#' @param effInt   -- percentage of efficacy stopping [interim]   
+#' @param effFin   -- percentage of efficacy stopping [final]     
+#' @param true_sd  -- true standard deviation in data generation model  
+#' @param true_mu1 -- true mean change in control arm 
+#' @param true_mu0 -- true mean change in treated arm
+#' @param a0       -- deterministic power prior parameter  
 #' @export 
 SIM <- function(...){
     r <- SIM_RAW(...);
